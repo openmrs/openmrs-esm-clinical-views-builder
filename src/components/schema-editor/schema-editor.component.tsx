c872353b5ad25b558e00e34e7db7fd6a7ebe26e6
hadijahkyampeire <hadijah315@gmail.com>
2024-09-26 03:20:14 +0300

    remove react-router-dom

--- src/components/schema-editor/schema-editor.component.tsx
+++ src/components/schema-editor/schema-editor.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import AceEditor from 'react-ace';
 import 'ace-builds/webpack-resolver';
 import 'ace-builds/src-noconflict/ext-language_tools';
